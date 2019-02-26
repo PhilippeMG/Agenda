4971cb788d1dba0f7f50261d0e62b5a860870c5c
al375909 <al375909@uji.es>
2019-02-26 13:45:55 +0100

    Constructor Empresa y Particular.

--- src/main/java/agenda/Empresa.java
+++ src/main/java/agenda/Empresa.java
@@ -1,4 +1,7 @@
 package agenda;
 
 public class Empresa extends Cliente {
+    public Empresa(String nombre, String nif) {
+        super(nombre, nif);
+    }
 }
--- src/main/java/agenda/Particular.java
+++ src/main/java/agenda/Particular.java
@@ -2,4 +2,9 @@ package agenda;
 
 public class Particular extends Cliente{
     String apellidos;
+
+    public Particular(String nombre, String nif, String apellidos) {
+        super(nombre, nif);
+        this.apellidos = apellidos;
+    }
 }
