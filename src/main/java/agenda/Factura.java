258c094191d3e614ffeb4d5e60f9d69b757ea538
philippePego95 <p.gonzalezmiralles@gmail.com>
2019-02-28 13:16:14 +0100

    metodo buscarFactura, recuperarFacturasCLientes

--- src/main/java/agenda/Factura.java
+++ src/main/java/agenda/Factura.java
@@ -1,6 +1,7 @@
 package agenda;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Factura {
@@ -10,6 +11,7 @@ public class Factura {
     Date periodo;
     Double importe;
 
+
     public Factura(int tipoTarifa, int cod, Date emision, Date periodo, Double importe) {
         this.tipoTarifa = tipoTarifa;
         this.cod = cod;
@@ -17,7 +19,17 @@ public class Factura {
         this.periodo = periodo;
         this.importe = importe;
     }
-    public void calcularFactura(Date inicio, Date finalizacion, LinkedList llamadas, LinkedList facturas){
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "tipoTarifa=" + tipoTarifa +
+                ", cod=" + cod +
+                ", emision=" + emision +
+                ", periodo=" + periodo +
+                ", importe=" + importe +
+                '}';
+    }
+    public void calcularFactura(Date inicio, Date finalizacion, LinkedList llamadas, LinkedList facturas, HashMap<Integer, Factura> factures){
         double valor=0;
         for(int i=0; i<llamadas.size();i++){
             Llamada llamada= (Llamada) llamadas.get(i);
@@ -29,6 +41,13 @@ public class Factura {
         //Como hacemos lo del periodo??
         Factura factura = new Factura(tipoTarifa,2,new Date(),inicio,valor);
         facturas.add(factura);
+        factures.put((int)factura.cod,factura);
 
+    }
+    public Factura buscarFactura(HashMap<String, Factura> facturas,String codigo){
+       return facturas.get(codigo);
+    }
+    public LinkedList<Factura> recuperarFacturasCliente(Cliente cliente){
+        return cliente.facturas;
     }
 }
