c8d39950f3e283107328ac442a6e19294ddab811
philippePego95 <p.gonzalezmiralles@gmail.com>
2019-03-04 22:16:47 +0100

    creación test

---
+++ src/test/java/agenda/ClienteTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertEquals;
+
+public class ClienteTest{
+
+}
