bae85718b365380c3f3b382d6a9512efe33a08bf
philippePego95 <p.gonzalezmiralles@gmail.com>
2019-03-05 12:50:44 +0100

    allTest

---
+++ src/main/java/agenda/test/AllTest.java
@@ -0,0 +1,9 @@
+package agenda.test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClienteTest.class,/* FacturaTest.class,*/GestorTest.class })
+
+public class AllTest {
+}
