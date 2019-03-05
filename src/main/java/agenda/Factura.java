7a7574f4c1062f1014ad956878ddb0795f46520d
philippePego95 <p.gonzalezmiralles@gmail.com>
2019-03-05 13:14:46 +0100

    funciona facturaTest

--- src/main/java/agenda/Factura.java
+++ src/main/java/agenda/Factura.java
@@ -1,6 +1,9 @@
 package agenda;
 
+import groovy.ui.SystemOutputInterceptor;
+
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -29,11 +32,13 @@ public class Factura {
     }
 
     public Double getImporte() {
+        if (importe==null) return 0.0;
         return importe;
     }
 
     public Double importe(Cliente cliente, LocalDate inicio, LocalDate fin) {
         double importe=0; //€/min
+        if (cliente==null || cliente.llamadas== null) return 0.0;
         for (Llamada llamada : cliente.llamadas) {
             //si es igual a inicio o a fin o posterior a inicio o anteror a fin.
             if((llamada.fechaLlamada.equals(inicio)  || llamada.fechaLlamada.isAfter(inicio)) && (llamada.fechaLlamada.equals(fin) || llamada.fechaLlamada.isBefore(fin))){
@@ -51,5 +56,21 @@ public class Factura {
                 ", inicio=" + this.inicio +
                 ", fin=" + this.fin +
                 ", importe=" + this.importe;
+    }
+
+    public static void main(String[] args) {
+        Direccion direccion1 = new Direccion(1234, "Valencia", "Burjassot");
+        Cliente cliente = new Cliente("Marcos", "0001", direccion1, "al375909@uji.es", 1);
+        Llamada llamada3 = new Llamada(654078311,0.9, LocalDate.of(2017, Month.MARCH, 1));
+        cliente.añadirLlamada(llamada3);
+        Factura factura = new Factura(cliente,LocalDate.of(2017,Month.MARCH, 1),LocalDate.of(2019,Month.MARCH, 3));
+        System.out.println("--Probando metodos--");
+        System.out.println(factura.toString());
+        System.out.println("--Probando getImporte--");
+        System.out.println(factura.getImporte());
+        System.out.println("--Probando Importe--");
+        System.out.println(factura.getImporte());
+        System.out.println("--Probando getCode--");
+        System.out.println(factura.getCod());
     }
 }
--- src/main/java/agenda/test/AllTest.java
+++ src/main/java/agenda/test/AllTest.java
@@ -3,7 +3,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ClienteTest.class,/* FacturaTest.class,*/GestorTest.class })
+@Suite.SuiteClasses({ ClienteTest.class,FacturaTest.class,GestorTest.class })
 
 public class AllTest {
 }
