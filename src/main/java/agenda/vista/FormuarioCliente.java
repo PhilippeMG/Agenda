b1329683d850f39d2ba5a071e27959898bf22ba2
al375923 <al375923@uji.es>
2019-05-03 12:28:28 +0200

    formulario vasico ampliado, falta ordenar

--- src/main/java/agenda/vista/FormuarioCliente.java
+++ src/main/java/agenda/vista/FormuarioCliente.java
@@ -8,11 +8,27 @@ public class FormuarioCliente {
         JFrame formulario=new JFrame("Añadir Cliente");//Creamos el JFrame
         Image icono = Toolkit.getDefaultToolkit().getImage("src/media/add-user.png"); //Creamos una IMAGE
         formulario.setIconImage(icono); //Añadimos la IMAGE creada
+        JTextField nombre = new JTextField(20);
+        JTextField dni = new JTextField(20);
+        JTextField direccion = new JTextField(20);
+        JTextField correo = new JTextField(20);
+        JTextField apellido = new JTextField(20);
+
+        JButton bAñadir=new JButton("Añadir");
+        //nombre, nif, direccion, correo, tarifa, apellidos
         Container contenedor = formulario.getContentPane();
         contenedor.setLayout(new FlowLayout());
-        contenedor.add(new JLabel("Nombre del Usuario:"));
-        contenedor.add( new JTextField(20));
-        contenedor.add(new JButton("Añadir"));
+        contenedor.add(new JLabel("Nombre:"),BorderLayout.CENTER);
+        contenedor.add(nombre,BorderLayout.EAST);
+        contenedor.add(new JLabel("Apellido:"),BorderLayout.CENTER);
+        contenedor.add(apellido,BorderLayout.EAST);
+        contenedor.add(new JLabel("DNI:"),BorderLayout.CENTER);
+        contenedor.add(dni,BorderLayout.EAST);
+        contenedor.add(new JLabel("Dirección:"),BorderLayout.CENTER);
+        contenedor.add(direccion,BorderLayout.EAST);
+        contenedor.add(new JLabel("Correo:"),BorderLayout.CENTER);
+        contenedor.add(correo,BorderLayout.EAST);
+        contenedor.add(bAñadir,BorderLayout.SOUTH);
         formulario.setSize(400,400);//Definimos el tamaño
         formulario.setVisible(true);// hacemos la ventsana visibel
     }
--- src/main/java/agenda/vista/Ventana.java
+++ src/main/java/agenda/vista/Ventana.java
@@ -10,15 +10,15 @@ class Ventana {
         //Modificamos el icono de la ventana
         Image icono = Toolkit.getDefaultToolkit().getImage("src/media/icono.png"); //Creamos una IMAGE
         ventana.setIconImage(icono); //Añadimos la IMAGE creada
-
-        ventana.setSize(800,500);//Definimos el tamaño
-        ventana.setVisible(true);// hacemos la ventsana visibel
         JTabbedPane pestanyas = new JTabbedPane();
         pestanyas.add("Clentes", new PanelClientes());
         pestanyas.add("Facturas", new PanelFacturas());
         pestanyas.add("Llamadas", new PanelLlamadas());
 
         ventana.add(pestanyas);
+        ventana.setSize(800,500);//Definimos el tamaño
+        ventana.setVisible(true);// hacemos la ventsana visibel
+
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
