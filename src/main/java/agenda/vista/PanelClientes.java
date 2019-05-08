c0be622c395538b95e5894f19bb2d3bff0939184
al375923 <al375923@uji.es>
2019-05-08 10:58:45 +0200

    intento de actualizar la tabla

--- src/main/java/agenda/vista/PanelClientes.java
+++ src/main/java/agenda/vista/PanelClientes.java
@@ -5,6 +5,8 @@ import agenda.modelo.Modelo;
 import agenda.modelo.excepciones.ClientNotFound;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -35,21 +37,20 @@ public class PanelClientes extends JPanel {
         panelBorrar.add(dniCliente);
         panelBorrar.add(bBorrarCliente);
 
-        Vector columnas = new Vector();
-        columnas.add("NIF");
-        columnas.add("Nombre");
-        columnas.add("Dirección");
-        columnas.add("Correo");
-        columnas.add("Tarifa");
-        columnas.add("Fecha");
+        Vector cabecera = new Vector();
+        cabecera.add("NIF");
+        cabecera.add("Nombre");
+        cabecera.add("Dirección");
+        cabecera.add("Correo");
+        cabecera.add("Tarifa");
+        cabecera.add("Fecha");
 
-        Vector filas = modelo.informacionClientes(modelo.getClientes());
+        Vector datos = modelo.informacionClientes(modelo.getClientes());
 
 
-        JTable tabla = new JTable(filas, columnas);
+        JTable tabla = new JTable(datos, cabecera);
         JScrollPane panel = new JScrollPane(tabla);
-        panel.createHorizontalScrollBar();
-        panel.createVerticalScrollBar();
+
         bAñadirCliente.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Creamdo cliente...");
@@ -83,6 +84,7 @@ public class PanelClientes extends JPanel {
                 System.out.println("Guardando Datos...");
                 try {
                     controlador.guardarDatos();
+                //    actualizarTabla(tabla,datos);
 
                 } catch (IOException ex) {
                     ex.printStackTrace();
@@ -100,9 +102,13 @@ public class PanelClientes extends JPanel {
         panelOption.add(bEditarCliente);
         panelOption.add(bSave);
         contenedor.add(panelOption);
-        contenedor.add(panel, BorderLayout.SOUTH);
+        contenedor.add(panel);
         add(contenedor);
 
+    }
+    public void actualizarTabla(JTable tabla,Vector datos){
+        TableModelEvent evento= new TableModelEvent((TableModel) datos);
+        tabla.tableChanged(evento);
     }
 
 }
